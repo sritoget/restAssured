9e805e421339d1614dca262d7a15e57d8d3dc34f
Srikrishna <Srikrishna@Srikrishna-PC>
2018-10-26 16:03:13 +1100

    Formatting

--- restAssuredFramework/src/test/java/getRequest/GetData.java
+++ restAssuredFramework/src/test/java/getRequest/GetData.java
@@ -2,9 +2,9 @@ package getRequest;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
+import static org.hamcrest.Matchers.*;
 
 public class GetData {
 
@@ -12,25 +12,17 @@ public class GetData {
 	public void testResponseCode()
 	{
 		Response resp=get("https://spring-movies-cf.cfapps.io/");
-		
 		int code=resp.getStatusCode();
 		System.out.println("Response status code is: "+code);
-		Assert.assertEquals(code,200);
-		
+		Assert.assertEquals(code,200);	
 	}
-	
-	
 	
 	@Test
 	public void testDoesWonderWomenMovieImageExist()
 	{
 		Response resp=get("https://spring-movies-cf.cfapps.io/");
-		
 		String data=resp.asString();
 		System.out.println("Whole Data is : "+data);
-		
 		Assert.assertTrue(data.contains("wonderWoman.jpg"));
 	}
-	
-	
 }
